f9494cbb95cbbc23fa7eea180355f97a78accf93
DrFlowerkick <flowerkick14@gmail.com>
2025-07-18 12:43:13 +0200

    Fixing handling of sw.js and cache.

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    // Get Cargo.toml version
+    let version = env::var("CARGO_PKG_VERSION").unwrap();
+
+    // get Template-SW
+    let sw_template = fs::read_to_string("public/sw.template.js").expect("failed to read template");
+
+    // set {{VERSION}} in template
+    let sw_content = sw_template.replace("{{VERSION}}", &version);
+
+    // write template with version in file
+    fs::write("public/sw.js", sw_content).expect("failed to write sw.js");
+
+    println!("cargo:rerun-if-changed=public/sw.template.js");
+}
